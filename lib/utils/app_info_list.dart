48b797af5a1ae62aa284ee0ca79db8ef4ea58877
bdot100 <bidemiokunade@gmail.com>
2024-03-12 21:06:11 +0100

    Book Ticket App Started and Home page designed

---
+++ lib/utils/app_info_list.dart
@@ -0,0 +1,65 @@
+List<Map<String, dynamic>> hotelList = [
+  {
+    'image': 'one.jpeg',
+    'place': 'Open Space',
+    'destination': 'London',
+    'price': 25
+  },
+  {
+    'image': 'two.jpeg',
+    'place': 'Global Will',
+    'destination': 'London',
+    'price': 40
+  },
+  {
+    'image': 'three.jpeg',
+    'place': 'Tallest Building',
+    'destination': 'Dubai',
+    'price': 68
+  },
+];
+
+List<Map<String, dynamic>> ticketList = [
+  {
+    'from': {
+      'code': "NYC",
+      'name': "New-York"
+    },
+    'to': {
+      'code': "LDN",
+      'name': "London"
+    },
+    'flying_time': '8H 30M',
+    'date': "1 May",
+    'departure_time': "08:00 AM",
+    'number': 23
+  },
+  {
+    'from': {
+      'code': "DK",
+      'name': "Dhaka"
+    },
+    'to': {
+      'code': "SH",
+      'name': "Shangai"
+    },
+    'flying_time': '4H 20M',
+    'date': "1 May",
+    'departure_time': "09:00 AM",
+    'number': 45
+  },
+  {
+    'from': {
+      'code': "NYC",
+      'name': "New-York"
+    },
+    'to': {
+      'code': "LDN",
+      'name': "London"
+    },
+    'flying_time': '8H 30M',
+    'date': "1 May",
+    'departure_time': "08:00 AM",
+    'number': 23
+  },
+];
